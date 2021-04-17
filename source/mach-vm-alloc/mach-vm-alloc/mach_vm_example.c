e1bd40ff2df79a69598b206ec99baa463514961f
Faisal Memon <faisalmemonuk@yahoo.com>
2021-04-17 11:05:27 +0100

    clean up c code by using unions

--- source/mach-vm-alloc/mach-vm-alloc/mach_vm_example.c
+++ source/mach-vm-alloc/mach-vm-alloc/mach_vm_example.c
@@ -14,8 +14,10 @@
 
 int vm_example()
 {
-    char            *data1, *temp;
-    char            *data2;
+    union data_area {
+        char *indexed;
+        vm_address_t handle;
+    } data1, data2;
     int             i;
     uintptr_t       min;
     unsigned int    data_cnt;
@@ -26,7 +28,7 @@ int vm_example()
     printf("mach_task_self is %x", self);
     
     if ((rtn = vm_allocate(self,
-                           (vm_address_t *)&data1,
+                           &data1.handle,
                            vm_page_size,
                            TRUE)) != KERN_SUCCESS) {
         mach_error("vm_allocate failed", rtn);
@@ -34,14 +36,13 @@ int vm_example()
         return -1;
     }
     
-    temp = data1;
     for (i = 0; (i < vm_page_size); i++) {
-        temp[i] = i;
+        data1.indexed[i] = i;
     }
     printf("Filled space allocated with some data.\n");
     printf("Doing vm_read....\n");
     if ((rtn = vm_read(self,
-                       (vm_address_t)data1,
+                       data1.handle,
                        vm_page_size,
                        (pointer_t *)&data2,
                        &data_cnt)) != KERN_SUCCESS) {
@@ -58,7 +59,7 @@ int vm_example()
     min = (vm_page_size < data_cnt) ? vm_page_size : data_cnt;
     
     for (i = 0; (i < min); i++) {
-        if (data1[i] != data2[i]) {
+        if (data1.indexed[i] != data2.indexed[i]) {
             printf("vmread: Data not read correctly.\n");
             printf("vmread: Exiting.\n");
             return -1;
@@ -67,7 +68,7 @@ int vm_example()
     printf("Checked data successfully.\n");
     
     if ((rtn = vm_deallocate(self,
-                             (vm_address_t)data1,
+                             data1.handle,
                              vm_page_size)) != KERN_SUCCESS) {
         mach_error("vm_deallocate failed", rtn);
         printf("vmread: Exiting.\n");
@@ -75,7 +76,7 @@ int vm_example()
     }
     
     if ((rtn = vm_deallocate(self,
-                             (vm_address_t)data2,
+                             data2.handle,
                              data_cnt)) != KERN_SUCCESS) {
         mach_error("vm_deallocate failed", rtn);
         printf("vmread: Exiting.\n");
