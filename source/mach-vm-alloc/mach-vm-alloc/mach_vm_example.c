c7fd4583160fc35a648466e81d5e26d81cf1cd15
Faisal Memon <faisalmemonuk@yahoo.com>
2021-04-17 11:34:30 +0100

    modernize for types and error handling

--- source/mach-vm-alloc/mach-vm-alloc/mach_vm_example.c
+++ source/mach-vm-alloc/mach-vm-alloc/mach_vm_example.c
@@ -15,15 +15,18 @@
 int vm_example()
 {
     union data_area {
-        char *indexed;
-        vm_address_t handle;
+        char                *indexed;
+        vm_address_t        handle;
     } data1, data2;
-    int             i;
-    uintptr_t       min;
-    unsigned int    data_cnt;
-    kern_return_t   rtn;
     
-    mach_port_t self = mach_task_self();
+    vm_size_t               i;
+    vm_size_t               min;
+    mach_msg_type_number_t  data_cnt;
+    mach_port_t             self;
+    char                    *error = NULL;
+    kern_return_t           rtn = KERN_SUCCESS;
+    
+    self = mach_task_self();
     
     printf("mach_task_self is %x", self);
     
@@ -31,9 +34,8 @@ int vm_example()
                            &data1.handle,
                            vm_page_size,
                            TRUE)) != KERN_SUCCESS) {
-        mach_error("vm_allocate failed", rtn);
-        printf("vmread: Exiting.\n");
-        return -1;
+        error = "Could not vm_allocate";
+        goto vm_example_error_return;
     }
     
     for (i = 0; (i < vm_page_size); i++) {
@@ -44,25 +46,23 @@ int vm_example()
     if ((rtn = vm_read(self,
                        data1.handle,
                        vm_page_size,
-                       (pointer_t *)&data2,
+                       &data2.handle,
                        &data_cnt)) != KERN_SUCCESS) {
-        mach_error("vm_read failed", rtn);
-        printf("vmread: Exiting.\n");
-        return -1;
+        error = "Could not vm_read";
+        goto vm_example_error_return;
     }
     printf("Successful vm_read.\n");
     
     if (vm_page_size != data_cnt) {
-        printf("vmread: Number of bytes read not equal to number");
-        printf("available and requested.\n");
+        error = "vmread: Number of bytes read not equal to number available and requested.";
+        goto vm_example_logic_error_return;
     }
     min = (vm_page_size < data_cnt) ? vm_page_size : data_cnt;
     
     for (i = 0; (i < min); i++) {
         if (data1.indexed[i] != data2.indexed[i]) {
-            printf("vmread: Data not read correctly.\n");
-            printf("vmread: Exiting.\n");
-            return -1;
+            error = "Data not read correctly.";
+            goto vm_example_logic_error_return;
         }
     }
     printf("Checked data successfully.\n");
@@ -70,17 +70,23 @@ int vm_example()
     if ((rtn = vm_deallocate(self,
                              data1.handle,
                              vm_page_size)) != KERN_SUCCESS) {
-        mach_error("vm_deallocate failed", rtn);
-        printf("vmread: Exiting.\n");
-        return -1;
+        error = "Could not vm_deallocate";
+        goto vm_example_error_return;
     }
     
     if ((rtn = vm_deallocate(self,
                              data2.handle,
                              data_cnt)) != KERN_SUCCESS) {
-        mach_error("vm_deallocate failed", rtn);
-        printf("vmread: Exiting.\n");
-        return -1;
+        error = "Could not vm_deallocate";
+        goto vm_example_error_return;
     }
     return 0;
+    
+vm_example_error_return:
+    printf("%s: %s\n", error, mach_error_string(rtn));
+    return -1;
+
+vm_example_logic_error_return:
+    printf("%s\n", error);
+    return -1;
 }
