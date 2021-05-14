07fe35752ee0ffd3a35b381a91556c53100c5e5c
Faisal Memon <faisalmemonuk@yahoo.com>
2021-05-14 20:03:11 +0100

    add Book Builder

---
+++ source/BookBuilder/BookBuilder/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BookBuilder
+//
+//  Created by Faisal Memon on 14/05/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
