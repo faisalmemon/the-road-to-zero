9b735cccd77e858a6b53e555be355c6060a4f309
Faisal Memon <faisalmemonuk@yahoo.com>
2021-05-15 16:11:17 +0100

    disable the app sandbox since we are like a command line tool when we adjust the book files

--- source/BookBuilder/BuilderLibrary/Sources/BuilderLibrary/Trademarks.swift
+++ source/BookBuilder/BuilderLibrary/Sources/BuilderLibrary/Trademarks.swift
@@ -9,6 +9,37 @@ import Foundation
 
 public struct Trademarks {
     public static func findTrademarks(config: Configuration) -> String {
-        return ""
+        let trademarks = TrademarksInternal(configuration: config)
+        let result = trademarks.getIndexEntries()
+        if let gotResult = result?.first {
+            return gotResult
+        } else {
+            return ""
+        }
+    }
+}
+
+class TrademarksInternal {
+    let config: Configuration
+    let fileManager: FileManager
+    
+    init(configuration: Configuration) {
+        config = configuration
+        fileManager = FileManager.default
+    }
+    
+    func getIndexEntries() -> [String]? {
+        var indexFileURL = URL(fileURLWithPath: config.outputDir)
+        indexFileURL.appendPathComponent("boo.en.idx")
+        do {
+            let fullStringContents =
+                try String(contentsOf: indexFileURL, encoding: .utf8)
+            let stringArray =
+                fullStringContents.components(separatedBy: .newlines)
+            return stringArray
+        } catch {
+            print("Error info: \(error)")
+            return nil
+        }
     }
 }
