a3f03d6f608dd85e26d7c47fc977fea0954bb5b6
Faisal Memon <faisalmemonuk@yahoo.com>
2021-05-27 20:27:04 +0100

    start again the BookBuilder app but this time make it a document based app that exports a custom type, .bookbuilder, which is based upon json

---
+++ source/expt/BookBuilder/BookBuilder/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  BookBuilder
+//
+//  Created by Faisal Memon on 27/05/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
