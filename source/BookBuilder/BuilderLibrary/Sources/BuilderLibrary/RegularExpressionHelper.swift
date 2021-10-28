ceec4c32e1fc554df59dd55f27a19e4df675e80e
Faisal Memon <faisalmemonuk@yahoo.com>
2021-10-28 15:04:42 +0100

    Add code to chop out lines

--- source/BookBuilder/BuilderLibrary/Sources/BuilderLibrary/RegularExpressionHelper.swift
+++ source/BookBuilder/BuilderLibrary/Sources/BuilderLibrary/RegularExpressionHelper.swift
@@ -38,4 +38,38 @@ struct RegularExpressionHelper {
         }
     }
     
+    /// When a line matches, chop it out and the immediately following line.
+    ///
+    /// [Line Chopping from  StackOverflow](https://stackoverflow.com/questions/56080711/sed-delete-x-lines-after-a-match/56080831#56080831)
+    /// ```
+    /// sed -e '/if(csl-hanging-indent)/{N;d;}' -i.bak boo.$langName.latex
+    /// achieves this:
+    /// ➜  ios-crash-dump-analysis-book git:(master) diff boo.en.latex.bak boo.en.latex
+    /// 10418,10419d10417
+    /// <   {$if(csl-hanging-indent)$\setlength{\parindent}{0pt}%
+    /// <   \everypar{\setlength{\hangindent}{\cslhangindent}}\ignorespaces$endif$}%
+    ///```
+    /// That means when the csl-hanging-indent pattern is seen, chop out that line
+    /// an the following line.
+    static func chopOutMatchAndFollowingLine(contents: [String], pattern: String) -> [String] {
+        let regex = try! NSRegularExpression(pattern: pattern, options: [])
+        
+        var result = [String]()
+        var linesToIgnore = 0
+        for line in contents {
+            if linesToIgnore > 0 {
+                linesToIgnore -= 1
+                continue
+            }
+            let matchResult = regex.matches(in: line, options: [], range: rangeForString(line))
+            if matchResult.count > 0 {
+                print("got match")
+                linesToIgnore = 1
+                continue
+            } else {
+                result.append(line)
+            }
+        }
+        return result
+    }
 }
