5c0ae5cb619e52c61a358fd691aa154d7f3d64ff
Faisal Memon <faisalmemonuk@yahoo.com>
2022-12-23 09:52:04 +0000

    Add website covering simulation of ipod and iphone.

---
+++ external/sites/devos50.github.io/assets/js/zoom.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){medium_zoom=mediumZoom("[data-zoomable]",{margin:100,background:getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")+"ee"})});
